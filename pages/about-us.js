89c3f913b55671e6505a161338bc6cb778335e1c
batimovil <german.figueroa@ort.edu.ar>
2022-08-18 19:52:36 -0300

    cambios de texto

--- pages/about-us.js
+++ pages/about-us.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const about = () => {
-  return <div>Esta es la pagina del</div>;
+  return <div>Esta es la pagina del team Mimo and Richard Deluxe</div>;
 };
 
 export default about;
